9443b5835ec99aef6cb7f5995b694d5d995fafdd
hiygabo <gaboandiaalave@gmail.com>
2026-09-19 23:36:41 -0400

    Feat(module) Add Schedule module

---
+++ src/main/kotlin/com/github/hiygabo/tourguide/schedule/entities/Schedule.kt
@@ -0,0 +1,24 @@
+package com.github.hiygabo.tourguide.schedule.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import java.time.LocalTime
+
+@Entity
+@Table(name="SCHEDULE")
+class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @Column(name = "opening_time", nullable = false)
+    var openingTime: LocalTime? = null
+
+    @Column(name = "closing_time", nullable = false)
+    var closingTime: LocalTime? = null
+
+}
